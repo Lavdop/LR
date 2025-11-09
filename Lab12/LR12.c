ac430b3968e3fa76b982dd7175e46caa26a24a30
Lavdop <i.d.ivanenko123@gmail.com>
2025-11-09 17:56:41 +0300

    поменял 0 на 1

--- Lab12/LR12.c
+++ Lab12/LR12.c
@@ -3,15 +3,15 @@
 
 int main() {
     long long a;
-    bool b = 0;
+    bool b = 1;
     while (scanf("%lld", &a) > 0) {
-        b = 0;
+        b = 1;
         if (a < 0) {
             a = -a;
         }
         while (a > 0) {
             if (a % 10 == (a / 10) % 10) {
-                b = 1;
+                b = 0;
                 break;
             } 
             a = a / 10;
