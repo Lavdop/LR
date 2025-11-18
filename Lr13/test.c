e8b93f7eb148e5c50bfcecf6a9c8328868fcd3c2
Lavdop <i.d.ivanenko123@gmaol.com>
2025-11-18 10:36:43 +0300

    nvim говно

--- Lr13/test.c
+++ Lr13/test.c
@@ -3,11 +3,37 @@
 void PrintUnicode(char* c){
     printf("%s", c);
     printf("\n");
+
+}
+unsigned int GetUnicode(){
+	int a = getchar();
+	int b = 0;
+	unsigned int res = 0;
+	for(int i = 7; i > 0; i--){
+		if(a >> i == 1){
+			b++;
+		} else{
+			break;
+		}
+	}
+	for (int i = 0; i < b; i++){
+		if(i == 0){
+			unsigned int t = 0;
+			for(int k = 0; k < (7 - b);k++){
+				t = t<<1 + 1;}
+			res = a & t;
+		}
+		
+		else{
+			a = getchar();
+			res = res<<6 + (a & 127); 
+
+	}
+
+	}
+	return res;
 }
 int main(){
-
-    unsigned char c = getchar();
-    printf("%u ", c);
-    c = getchar();
-    printf("%u ", c);
+	printf("%u", GetUnicode()); 
 }
+
